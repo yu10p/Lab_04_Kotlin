1309b7f77985654bc176ebc768e883d2d60e101d
yu10p <eason31003@gmail.com>
2022-11-01 21:10:35 +0800

    Lab_04_Kotlin

---
+++ app/src/main/java/com/example/lab_4_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.lab_4_kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tv_meal:TextView
+    private lateinit var btn:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //初始化Activity
+        super.onCreate(savedInstanceState)
+        //連接main1.xml畫面
+        setContentView(R.layout.activity_main)
+        //連接tv_meal元件
+        tv_meal = findViewById<TextView>(R.id.tv_meal)
+        //連接btn_choice
+        btn = findViewById<Button>(R.id.btn_choice)
+        //監聽btn_choice
+        btn.setOnClickListener(){
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivityForResult(intent,1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data == null) return
+
+        if (requestCode == 1) {
+            if (resultCode == 101) {
+                val b = data.extras
+                val str1 = b!!.getString("drink")
+                val str2 = b.getString("sugar")
+                val str3 = b.getString("ice")
+                tv_meal.text = String.format(
+                    "飲料: %s\n\n甜度: %s\n\n冰塊: %s\n\n",
+                    str1, str2, str3
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lab_4_kotlin/MainActivity2.kt
@@ -0,0 +1,56 @@
+package com.example.lab_4_kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var send_btn: Button
+    private lateinit var set_drink: EditText
+    private lateinit var rg1: RadioGroup
+    private lateinit var rg2: RadioGroup
+
+    private var sugar = "無糖"
+    private var ice_opt = "去冰"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        rg1 = findViewById(R.id.radioGroup)
+        rg1.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { radioGroup, i ->
+            when (i) {
+                R.id.radioButton1 -> sugar = "無糖"
+                R.id.radioButton2 -> sugar = "少糖"
+                R.id.radioButton3 -> sugar = "半糖"
+                R.id.radioButton4 -> sugar = "全糖"
+            }
+        })
+        rg2 = findViewById<RadioGroup>(R.id.radioGroup2)
+        rg2.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { radioGroup, i ->
+            when (i) {
+                R.id.radioButton5 -> ice_opt = "去冰"
+                R.id.radioButton6 -> ice_opt = "微冰"
+                R.id.radioButton7 -> ice_opt = "少冰"
+                R.id.radioButton8 -> ice_opt = "正常冰"
+            }
+        })
+        send_btn = findViewById(R.id.btn_send)
+        send_btn.setOnClickListener(View.OnClickListener {
+            set_drink = findViewById(R.id.ed_drink)
+            val drink = set_drink.getText().toString()
+            val i = Intent()
+            val b = Bundle()
+            b.putString("sugar", sugar)
+            b.putString("drink", drink)
+            b.putString("ice", ice_opt)
+            i.putExtras(b)
+            setResult(101, i)
+            finish()
+        })
+    }
+}
